854fe5554275799485d6626b496a8d23be603691
Archana-ks <achu24ks@gmail.com>
2023-06-21 22:33:36 +0530

    cors 1

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://version-control-system-fe.netlify.app",
     credentials: true,
   })
 );
