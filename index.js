7adfefa43a8e26fd695957e265e70b85977e56e0
Archana-ks <achu24ks@gmail.com>
2023-06-21 22:02:00 +0530

    cors

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
+  cors()
 );
 
 app.get("/", (req, res) => {
